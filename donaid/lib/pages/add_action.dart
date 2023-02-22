95c088a2d877c527f5239b6100ca4fc7a498b8cf
vasileiantaliani <ntalianiv@gmail.com>
2023-02-22 12:27:07 +0200

    start date, end date, datepicker, first try for category list

--- donaid/lib/pages/add_action.dart
+++ donaid/lib/pages/add_action.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
+import 'package:intl/intl.dart';
+
+
 
 class TextFieldWidgetAct extends StatefulWidget {
   final int maxLines;
@@ -144,14 +147,30 @@ class AddActionPage extends StatefulWidget {
 var _controllerT = TextEditingController();
 var _controllerC = TextEditingController();
 
+TextEditingController startdateController= TextEditingController();
+TextEditingController enddateController= TextEditingController();
+  final TextEditingController _controller = new TextEditingController();
+  
+
+
 class _AddActionPageState extends State<AddActionPage> {
 
-  bool _switchValue = false;
+bool _switchValue = false;
+var items = ['Τρόφιμα', 'Φάρμακα', 'Αιμοδοσία', 'Αδέσποτα'];
+
+// var items = <your call to api>;
+@override
+void initState(){
+  super.initState();
+  startdateController.text="";
+  enddateController.text = "";
+}
+
   @override
   Widget build(BuildContext context) => Builder(
         builder: (context) => ListView(
           padding: EdgeInsets.symmetric(horizontal: 32),
-          physics: BouncingScrollPhysics(),
+          physics: BouncingScrollPhysics(),          
           children: [
             ActionWidget(
               imagePath:
@@ -159,6 +178,7 @@ class _AddActionPageState extends State<AddActionPage> {
               isEdit: true,
               onClicked: () async {},
             ),
+
             const SizedBox(height: 24),
             TextFormField(
               decoration: InputDecoration(
@@ -173,6 +193,7 @@ class _AddActionPageState extends State<AddActionPage> {
               ),
               onChanged: (description) {},
             ),
+
             const SizedBox(height: 24),
             TextFormField(
               controller: _controllerT,
@@ -186,15 +207,99 @@ class _AddActionPageState extends State<AddActionPage> {
               ),
               onChanged: (location) {},
             ),
-            const SizedBox(height: 24),
-            TextFormField(
-              enabled: _switchValue,
-              decoration: InputDecoration(
-                labelText: "Ημερομηνία",
-                hintText: 'dd/mm/yyyy'
-              ),
-              onChanged: (date) {},
-            ),
+            
+            // const SizedBox(height: 24),
+            // TextFormField(
+            //   enabled: _switchValue,
+            //   decoration: InputDecoration(
+            //     labelText: "Ημερομηνία",
+            //     hintText: 'dd/mm/yyyy'
+            //   ),
+            //   onChanged: (date) {},
+            // ),
+
+           Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: <Widget>[
+ 
+    const SizedBox(width: 20.0, height: 24,),
+     Flexible(  
+      child:  TextFormField(
+        enabled: _switchValue,
+        controller: startdateController,
+          decoration: InputDecoration(
+            //  contentPadding: EdgeInsets.all(10),
+              labelText: "Start date",
+              icon: Icon(Icons.calendar_today),
+              
+          ),
+          readOnly: true,
+          onTap: () async {
+                  //when click we have to show the datepicker
+                  DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                       initialDate: DateTime.now(), //get today's date
+                      firstDate:DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                      lastDate: DateTime(2101)
+                  );
+                    if(pickedDate != null ){
+                     // print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
+                      String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
+                     // print(formattedDate); //formatted date output using intl package =>  2022-07-04
+                        //You can format date as per your need
+
+                      setState(() {
+                          startdateController.text = formattedDate; //set foratted date to TextField value. 
+                        //startdateController.text = pickedDate.toString();
+                      });
+                  }else{
+                      print("Date is not selected");
+                  }
+            }
+        
+      ),
+      
+    ),
+    const SizedBox(width: 20.0, height: 24,),
+     Flexible(
+      child: TextFormField(
+        enabled: _switchValue,
+        controller: enddateController,
+          decoration: InputDecoration(
+            //  contentPadding: EdgeInsets.all(10),
+              labelText: "End date",
+              icon: Icon(Icons.calendar_today),
+          ),
+          readOnly: true,
+          onTap: () async {
+                  //when click we have to show the datepicker
+                  DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                       initialDate: DateTime.now(), //get today's date
+                      firstDate:DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                      lastDate: DateTime(2101)
+                  );
+                    if(pickedDate != null ){
+                     // print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
+                      String formattedDate = DateFormat('dd-MM-yyyy').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
+                     // print(formattedDate); //formatted date output using intl package =>  2022-07-04
+                        //You can format date as per your need
+
+                      setState(() {
+                          enddateController.text = formattedDate; //set foratted date to TextField value. 
+                        //startdateController.text = pickedDate.toString();
+                      });
+                  }else{
+                      print("Date is not selected");
+                  }
+            }
+        
+      ),
+    ),
+  ],
+),
+
+
             Switch(
               value: _switchValue,
               onChanged: (bool value) {
@@ -203,19 +308,45 @@ class _AddActionPageState extends State<AddActionPage> {
                 });
               },
             ),
-            const SizedBox(height: 24),
-            TextFormField(
-              controller: _controllerC,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: IconButton(
-                    onPressed: _controllerC.clear,
-                    icon: Icon(Icons.clear),
-                  ),
-                  labelText: "Κατηγορίες",
-                  hintText: 'Προσθήκη'),
-              onChanged: (categories) {},
-            ),
+
+            // const SizedBox(height: 24),
+            // TextFormField(
+            //   controller: _controllerC,
+            //   decoration: InputDecoration(
+            //       prefixIcon: Icon(Icons.search),
+            //       suffixIcon: IconButton(
+            //         onPressed: _controllerC.clear,
+            //         icon: Icon(Icons.clear),
+            //       ),
+            //       labelText: "Κατηγορίες",
+            //       hintText: 'Προσθήκη'),
+            //   onChanged: (categories) {},
+            // ),
+ 
+
+TextFormField(
+  controller: _controller,
+  decoration: InputDecoration(
+    suffixIcon: PopupMenuButton<String>(
+      icon: const Icon(Icons.arrow_drop_down),
+      onSelected: (String value) {
+        _controller.text = value;
+      },
+      itemBuilder: (BuildContext context) {
+        return items
+            .map<PopupMenuItem<String>>((String value) {
+          return new PopupMenuItem(
+              child: new Text(value), value: value);
+        }).toList();
+      },
+    ),
+    labelText: "Κατηγορίες",
+    hintText: 'Προσθήκη'
+  ),
+  
+),
+
+
             const SizedBox(height: 24),
             TextFormField(
               decoration: InputDecoration(
