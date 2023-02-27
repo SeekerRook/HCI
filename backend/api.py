e98fa68b8ed178f97945565876f70f6ee9ffdc53
SeekerRook <xrist.vardakis@gmail.com>
2023-02-28 00:11:36 +0200

    updated backend'

--- backend/api.py
+++ backend/api.py
@@ -5,10 +5,37 @@ app = Flask(__name__)
 
 @app.route("/actions/")
 def hello_world():
-        return json.load(open("actions.json"))
+        data =  json.load(open("./mysite/actions.json"))
+        print(data)
+        return data
 @app.route('/actions/post', methods=[ 'POST'])
 def question():
         if request.method == 'POST':
             data = (request.json)
-            json.dump(data,open("actions.json",'w'))
+            json.dump(data,open("./mysite/actions.json",'w'))
+            print(data)
+            return data
+@app.route("/test/actions/")
+def hello_worldt():
+        data =  json.load(open("./mysite/test_actions.json"))
+        print(data)
+        return data
+@app.route('/test/actions/post', methods=[ 'POST'])
+def questiont():
+        if request.method == 'POST':
+            data = (request.json)
+            json.dump(data,open("./mysite/test_actions.json",'w'))
+            print(data)
+            return data
+@app.route("/users/")
+def hello_world2():
+        data =  json.load(open("./mysite/users.json"))
+        print(data)
+        return data
+@app.route('/users/post', methods=[ 'POST'])
+def question2():
+        if request.method == 'POST':
+            data = (request.json)
+            json.dump(data,open("./mysite/users.json",'w'))
+            print(data)
             return data
